9f01d4ca5fb10110bb1ef1b4e1b2e5cfc0d4ab8d
Max Adams <max.c.aams@gmail.com>
2022-07-19 10:04:38 -0400

    for testing

--- src/wf_consumer/tests/test_wf_consumer.py
+++ src/wf_consumer/tests/test_wf_consumer.py
@@ -1,26 +1,36 @@
 import csv
 import json
+import os
 
 import pytest
-
 from wf_consumer import add_data, create_file
 
 
 def test_headers():
-    with open('output.json') as file_in:
+    with open("output.json") as file_in:
         body = json.load(file_in)
-        transactions = body['Transactions']['PaymentInstructions']
-        with create_file() as file: # use with as here because create_file opens fd
+        transactions = body["Transactions"]["PaymentInstructions"]
+        with create_file() as file:  # use with as here because create_file opens fd
             add_data(transactions=transactions, file=file)
             file.seek(0)
             reader = csv.reader(file)
-            ref_headers = ['BatchID', 'SequenceID', 
-                            'BatchDate', 'PolicyNum', 
-                            'PayeeFullName', 'PayeeStreet', 
-                            'PayeeCity', 'PayeeState', 
-                            'PayeeZipcode', 'Account', 
-                            'Comments', 'Amount', 
-                            'Currency', 'SourceSystem']
+            ref_headers = [
+                "BatchID",
+                "SequenceID",
+                "BatchDate",
+                "PolicyNum",
+                "PayeeFullName",
+                "PayeeStreet",
+                "PayeeCity",
+                "PayeeState",
+                "PayeeZipcode",
+                "Account",
+                "Comments",
+                "Amount",
+                "Currency",
+                "SourceSystem",
+            ]
             headers = next(reader)
             for header in headers:
                 assert header in ref_headers
+        os.remove(file.name)
